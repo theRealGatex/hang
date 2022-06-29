06e4662de4c20ade4f7481bbf768d08422c86364
theRealGatex <107705414+theRealGatex@users.noreply.github.com>
2022-06-29 20:18:36 -0300

    Update hang.js

--- hang.js
+++ hang.js
@@ -1,90 +1,122 @@
-//elige palabra y crea array para letras adivinadas
-let palabraOculta = prompt("Escribi una palabra")
+let palabraOculta = "";
 
-const letrasAdivinadas = []
-const estaLetraYaFueAdivinada = (letra) => letrasAdivinadas.includes(letra) //funcion que te dice si una letra ya se adivino o todavia no
-const winSong = document.getElementById("winSong")
+if (!window.location.search) {
+  while (palabraOculta == "") {
+    palabraOculta = prompt("Escribi una palabra");
+  }
+  ocultar("label");
 
-//te muestra cuantos caracteres tiene
-document.getElementById("cantidadDeLetras").innerHTML = "la palabra tiene " + palabraOculta.length + " letras"
+} else {
+  palabraOculta = atob(window.location.search.replace("?", ""));
+  ocultar("modoAdivinar");
+  ocultar("palabraDevelada")
+}
+document.getElementById("palabraDevelada").innerText = "La palabra es: " + palabraOculta
 
-//crea 1 "?" x cada caracter y divide la palabra en caracteres para luego matchear
-const head = document.createElement("h1");
-head.style.cssText = 'text-align: center;';
-document.body.appendChild(head);
+const newUrl = window.location.href + "?" + btoa(palabraOculta);
 
-const chars = palabraOculta.toLowerCase().split(''); // para mostrar array sin comas va .join(' ')
+const letrasAdivinadas = [];
+const estaLetraYaFueAdivinada = (letra) => letrasAdivinadas.includes(letra);
+
+const winSong = document.getElementById("winSong");
+
+document.getElementById("cantidadDeLetras").innerHTML =
+  "la palabra tiene " + palabraOculta.length + " letras";
+
+const palabraOcultaDisplay = document.getElementById("palabraOcultaDisplay");
+
+const caracteresPalabraOculta = palabraOculta.toLowerCase().split("");
 const regex = /[a-z]/gi;
+let strikes = 0;
 
-// muestra los caracteres ocultos de la palabra, crea un contador para contar los strikes y un lugar donde aparece el wachin
-
-head.innerText = palabraOculta.replace(regex, '*')
-let strikes = 0
-
-let wachin = document.createElement("img");
-wachin.style.cssText = ' max-width:10%;';
-document.body.appendChild(wachin);
-
-//cuando jugador escribe ve si la letra coincide o no con alguna de las letras de la palabraOculta
-document.getElementById("input").onchange = function () {
-    const userChar = document.getElementById("input").value.toLowerCase()
-    function checkChar() {
-        for (let i = 0; i < chars.length; i++) {
-            if (chars[i] == userChar) {
-                return true
-            }
-        }
-    }
-    if (checkChar() === true) {   // aca va lo que pasa si acertaste
-        letrasAdivinadas.push(userChar)
-        head.innerText = chars.map(caracter => estaLetraYaFueAdivinada(caracter) ? caracter : "*").join("")
-        if (head.innerText === palabraOculta) {
-
-            let win = document.createElement("img");
-            win.style.cssText = ' max-width:10%;';
-            document.body.appendChild(win);
-            win.src = "https://media4.giphy.com/media/SsaGzaI7QEaft8s3HK/giphy.gif?cid=790b7611ff284530e4067afd1cc70a4b122652e7530d7016&rid=giphy.gif&ct=s";
-            winSong.volume = 0.2;
-            winSong.play();
-            function winFunction() {
-            setTimeout(function () { location.reload(); }, 3000);
-        }
-        winFunction()
-        }
-        
-    } else { //aca va lo que pasa si no acertas
-
-        strikes++
-
-        switch (strikes) {
-            case 1:
-                wachin.src = "https://i.ibb.co/1z2hRwP/1-cara.png";
-                document.getElementById('letrasErradas').textContent += userChar;
-
-                break;
-            case 2:
-                wachin.src = "https://i.ibb.co/jVHzL7G/2-torso.png";
-                document.getElementById('letrasErradas').textContent += userChar;
-                break;
-            case 3:
-                wachin.src = "https://i.ibb.co/4m11Tv6/3-brazos.png";
-                document.getElementById('letrasErradas').textContent += userChar;
-                break;
-            case 4:
-                wachin.src = "https://i.ibb.co/6tmStks/4-patas.png";
-                document.getElementById('letrasErradas').textContent += userChar;
-                break;
-            case 5:
-                wachin.src = "https://i.ibb.co/WgfC3fH/5-soga.png";
-                function timeFunction() {
-                    setTimeout(function () { alert("La palabra era " + palabraOculta); location.reload(); }, 800);
-                }
-                timeFunction()
-                break;
-        }
-
-    }
-    document.getElementById("input").value = ""
+let wachin = document.getElementById("img");
+function ocultar(cosa) {
+  document.getElementById(cosa).style.display = "none";
+}
+function modoAdivinar() {
+  window.open(newUrl);
 }
 
+function setPalabraOcultaDisplay(text) {
+  palabraOcultaDisplay.innerText = text;
+}
+function reloadPage() {
+  window.open(window.location.pathname, "_self");
+}
+setPalabraOcultaDisplay(palabraOculta.replace(regex, "*"));
+function winFunction() {
+  setTimeout(function () {
+    reloadPage();
+  }, 3000);
+}
+function procesarLetraAdivinada(userChar) {
+  letrasAdivinadas.push(userChar);
+  setPalabraOcultaDisplay(
+    caracteresPalabraOculta
+      .map((caracter) => (estaLetraYaFueAdivinada(caracter) ? caracter : "*"))
+      .join("")
+  );
+  if (palabraOcultaDisplay.innerText === caracteresPalabraOculta.join("")) {
+    let win = document.createElement("img");
+    win.style.cssText = " max-width:10%;";
+    document.body.appendChild(win);
+    win.src =
+      "https://media4.giphy.com/media/SsaGzaI7QEaft8s3HK/giphy.gif?cid=790b7611ff284530e4067afd1cc70a4b122652e7530d7016&rid=giphy.gif&ct=s";
+    winSong.volume = 0.2;
+    winSong.play();
 
+    winFunction();
+  }
+}
+function looseFunction() {
+  setTimeout(function () {
+    alert("La palabra era " + palabraOculta);
+    reloadPage();
+  }, 800);
+}
+function procesarLetraErrada(userChar) {
+  strikes++;
+
+  switch (strikes) {
+    case 1:
+      wachin.src = "https://i.imgur.com/KbeRPVw.png";
+      document.getElementById("letrasErradas").textContent += userChar;
+
+      break;
+    case 2:
+      wachin.src = "https://i.imgur.com/t0fZfcT.png";
+      document.getElementById("letrasErradas").textContent += userChar;
+      break;
+    case 3:
+      wachin.src = "https://i.imgur.com/bz0lL9y.png";
+      document.getElementById("letrasErradas").textContent += userChar;
+      break;
+    case 4:
+      wachin.src = "https://i.imgur.com/qIBhe5c.png";
+      document.getElementById("letrasErradas").textContent += userChar;
+      break;
+    case 5:
+      wachin.src = "https://i.imgur.com/DCk0cDV.png";
+
+      looseFunction();
+      break;
+  }
+}
+function getUserChar() {
+  return document.getElementById("input").value.toLowerCase();
+}
+function cleanUserChar() {
+  document.getElementById("input").value = "";
+}
+
+document.getElementById("input").onchange = function () {
+  const userChar = getUserChar();
+  const adivinoLaLetra = caracteresPalabraOculta.includes(userChar);
+
+  if (adivinoLaLetra) {
+    procesarLetraAdivinada(userChar);
+  } else {
+    procesarLetraErrada(userChar);
+  }
+  cleanUserChar();
+};
